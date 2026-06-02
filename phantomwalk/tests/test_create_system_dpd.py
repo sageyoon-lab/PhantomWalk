38615e95684a50073897f67649a9636ad84a347b
ced4rtree <usernamesarenoteasy@proton.me>
2026-06-02 10:44:00 -0600

    use rm_files helper to remove log files

--- phantomwalk/tests/test_create_system_dpd.py
+++ phantomwalk/tests/test_create_system_dpd.py
@@ -9,6 +9,14 @@ sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'src')))
 import create_system_dpd as dpd
 
+# helper to not have several repetitive try/catch blocks for cleanup
+def rm_files(*files):
+    for fl in files:
+        try:
+            os.remove(fl)
+        except:
+            pass
+
 def test_creation():
     s = dpd.create_polymer_system_dpd(num_pol=5, num_mon=10, density=0.5)
     assert s > 0
@@ -16,9 +24,7 @@ def test_creation():
 def test_custom_log_files():
     # remove files that might've been output by other tests so that file loading
     # can fail if it's wrong
-    with suppress(FileNotFoundError):
-        os.remove('trajectory.gsd')
-        os.remove('log.txt')
+    rm_files('log.txt', 'trajectory.gsd')
 
     time_string = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     gsd_file_name = f"{time_string}.gsd"
@@ -30,8 +36,11 @@ def test_custom_log_files():
         gsd_file_name=gsd_file_name,
         log_file_name=log_file_name
     )
-    assert os.path.isfile(gsd_file_name) and os.path.isfile(log_file_name)
+
+    gsd_exists = os.path.isfile(gsd_file_name)
+    log_exists = os.path.isfile(log_file_name)
 
     # clean up after ourselves
-    os.remove(gsd_file_name)
-    os.remove(log_file_name)
+    rm_files(gsd_file_name, log_file_name)
+
+    assert gsd_exists and log_exists
